f8196554316745ca70fc8802d6cefe66a453f192
orgkampung-hub <akulasboss@gmail.com>
2026-01-07 20:36:22 +0800

    Add files via upload

--- utama.js
+++ utama.js
@@ -1,9 +1,11 @@
 function pilihGame(jenis) {
+    console.log("Butang ditekan: " + jenis); // Untuk semakan di Console (F12)
+
     if (jenis === 'cari-perkataan') {
-        // Navigasi ke projek GitHub anda
         window.location.href = "https://orgkampung-hub.github.io/Cari-perkataan/";
-    } else if (jenis === 'bendera') {
-        // Navigasi ke projek Game Bendera anda
+    } 
+    else if (jenis === 'bendera') {
+        // Pastikan URL ini tepat (Huruf Besar/Kecil sangat penting di GitHub)
         window.location.href = "https://orgkampung-hub.github.io/Bendera-dunia/";
     }
 }
