1c0cfa45f086f82a409ad00804e7ab8ace303e19
orgkampung-hub <akulasboss@gmail.com>
2026-01-07 19:35:04 +0800

    Add files via upload

---
+++ utama.js
@@ -0,0 +1,10 @@
+function pilihGame(jenis) {
+    if (jenis === 'cari-perkataan') {
+        // Navigasi ke projek GitHub anda
+        window.location.href = "https://orgkampung-hub.github.io/Cari-perkataan/";
+    } else if (jenis === 'bendera') {
+        alert("Modul Game Bendera akan datang tidak lama lagi!");
+    }
+}
+
+console.log("Portal Projek Akulas: Tema Gelap Lembut diaktifkan.");
